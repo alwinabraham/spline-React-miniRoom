9b28d6a80984b353b386aed7132e98ee9a10357d
alwinabraham <alwinabraham098@gmail.com>
2023-06-13 05:23:00 +0530

    3d spline practice

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/G1bzxReu6U6QGSie/scene.splinecode" />
+  );
+}
+
